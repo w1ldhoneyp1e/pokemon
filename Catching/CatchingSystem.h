c5a4690f49e6d5b504febc579f8ba1ea58f16069
w1ldhoneyp1e <k_yashmetov@mail.ry>
2024-11-29 19:55:02 +0300

    Разнес логику по функциям

--- Catching/CatchingSystem.h
+++ Catching/CatchingSystem.h
@@ -13,6 +13,14 @@ void throwPokeball(EntityManager* entityManager, float dt);
 void pokeballCollision(EntityManager* entityManager, RenderSystem* renderSystem, GameState* state);
 void removeCatchingEntities(EntityManager* entityManager, RenderSystem* renderSystem);
 
+void initCatchingLocation(EntityManager* entityManager);
+void initCatchingArrow(EntityManager* entityManager);
+void initCatchingPokeball(EntityManager* entityManager);
+void initCatchingPokemon(EntityManager* entityManager);
+
+void handlePokeballPokemonCollision(EntityManager* entityManager, RenderSystem* renderSystem, GameState* state);
+void handlePokeballOutOfMap(EntityManager* entityManager, RenderSystem* renderSystem, GameState* state);
+
 void updateCatching(
 	EntityManager* entityManager,
     InputSystem* inputSystem,
@@ -28,42 +36,57 @@ void updateCatching(
 }
 
 void initCatching(EntityManager* entityManager, RenderSystem* renderSystem) {
+    // TODO:  функцию сократить на 30 строк кода
     // Локация ловли
-    auto catchingLocation = entityManager->createEntity("catchingLocation");
-    catchingLocation->addComponent<PositionComponent>(0, 0);
-    catchingLocation->addComponent<SizeComponent>(WINDOW_WIDTH, WINDOW_HEIGHT);
-	catchingLocation->addComponent<RenderLayerComponent>(0);
-    catchingLocation->addComponent<CatchingTypeEntityComponent>();
-    sf::Texture catchingLocationTexture;
-    if (catchingLocationTexture.loadFromFile("../res/beach(383x640).png")) {
-        catchingLocation->addComponent<TextureComponent>(
-			catchingLocationTexture,
-			CATCHING_LOCATION_WIDTH, 
-			CATCHING_LOCATION_HEIGHT
-		);
-    }
-
+    initCatchingLocation(entityManager);
     // Стрелка
-    auto catchingArrow = entityManager->createEntity("catchingArrow");
-    catchingArrow->addComponent<PositionComponent>(
-        ARROW_POS_X, 
-        ARROW_POS_Y
-    );
-    catchingArrow->addComponent<SizeComponent>(ARROW_WIDTH, ARROW_HEIGHT);
-	catchingArrow->addComponent<RotationComponent>(ARROW_SPEED, ARROW_WIDTH / 2, ARROW_HEIGHT + ARROW_POKEBALL_GAP + POKEBALL_HEIGHT * 5 / 2);
-	catchingArrow->addComponent<RenderLayerComponent>(1);
-    catchingArrow->addComponent<CatchingTypeEntityComponent>();
-    sf::Texture catchingArrowTexture;
-    if (catchingArrowTexture.loadFromFile("../res/arrow(57x162).png")) {
-        catchingArrow->addComponent<TextureComponent>(
-			catchingArrowTexture,
-			ARROW_WIDTH, 
-			ARROW_HEIGHT
-		);
-    }
-    catchingArrow->getComponent<TextureComponent>()->sprite.setOrigin(ARROW_WIDTH / 2, ARROW_HEIGHT);
-
+    initCatchingArrow(entityManager);
     // Покебол
+    initCatchingPokeball(entityManager);
+    // Получить покемона, присвоить позицию и размеры
+    initCatchingPokemon(entityManager);
+
+    renderSystem->removeEntities();
+    renderSystem->addEntities(entityManager->getEntitiesWithComponent<CatchingTypeEntityComponent>());
+}
+
+void arrowRotation(EntityManager* entityManager, float dt) {
+    auto arrow = entityManager->getEntity("catchingArrow");
+    auto rotation = arrow->getComponent<RotationComponent>();
+    auto position = arrow->getComponent<PositionComponent>();
+
+    rotation->angle += dt * rotation->speed;
+    rotation->angle = std::fmod(rotation->angle, PI);
+
+    if (rotation->angle < ARROW_MIN_ANGLE) {
+        rotation->angle = ARROW_MIN_ANGLE;
+        rotation->speed *= -1;
+    } else if (rotation->angle > ARROW_MAX_ANGLE) {
+        rotation->angle = ARROW_MAX_ANGLE;
+        rotation->speed *= -1;
+    }
+
+    float x = ARROW_POS_X - ARROW_RADIUS * cosf(rotation->angle + PI / 2);
+    float y = ARROW_POS_Y - ARROW_RADIUS * sinf(rotation->angle + PI / 2);
+
+    position->setPos(x, y);
+}
+
+void initCatchingPokemon(EntityManager* entityManager) {
+    auto pokemon = entityManager->getEntitiesWithComponent<AttackedPokemonComponent>()[0];
+    pokemon->getComponent<PositionComponent>()->setPos(
+        WINDOW_WIDTH / 2.5,
+        WINDOW_HEIGHT / 10
+    );
+    pokemon->getComponent<SizeComponent>()->setSize(
+        WINDOW_WIDTH / 5,
+        WINDOW_WIDTH / 5
+    );
+    pokemon->addComponent<CatchingTypeEntityComponent>();
+}
+
+
+void initCatchingPokeball(EntityManager* entityManager) {
     auto pokeball = entityManager->createEntity("pokeball");
     pokeball->addComponent<PositionComponent>(
         POKEBALL_POS_X, 
@@ -83,46 +106,44 @@ void initCatching(EntityManager* entityManager, RenderSystem* renderSystem) {
 		);
     }
     pokeball->getComponent<TextureComponent>()->sprite.setOrigin(POKEBALL_WIDTH / 2, POKEBALL_HEIGHT / 2);
-
-    // Получить покемона, присвоить позицию и размеры
-    auto pokemon = entityManager->getEntitiesWithComponent<AttackedPokemonComponent>()[0];
-    pokemon->getComponent<PositionComponent>()->setPos(
-        WINDOW_WIDTH / 2.5,
-        WINDOW_HEIGHT / 10
-    );
-    pokemon->getComponent<SizeComponent>()->setSize(
-        WINDOW_WIDTH / 5,
-        WINDOW_WIDTH / 5
-    );
-    pokemon->addComponent<CatchingTypeEntityComponent>();
-
-    renderSystem->removeEntities();
-    renderSystem->addEntities(entityManager->getEntitiesWithComponent<CatchingTypeEntityComponent>());
 }
 
-void arrowRotation(EntityManager* entityManager, float dt) {
-    auto arrow = entityManager->getEntity("catchingArrow");
-    auto rotation = arrow->getComponent<RotationComponent>();
-    auto position = arrow->getComponent<PositionComponent>();
-
-    rotation->angle += dt * rotation->speed;
-    rotation->angle = std::fmod(rotation->angle, PI);
-
-    
-
-    if (rotation->angle < ARROW_MIN_ANGLE) {
-        rotation->angle = ARROW_MIN_ANGLE;
-        rotation->speed *= -1;
-    } else if (rotation->angle > ARROW_MAX_ANGLE) {
-        rotation->angle = ARROW_MAX_ANGLE;
-        rotation->speed *= -1;
+void initCatchingLocation(EntityManager* entityManager) {
+    initCatchingLocation(entityManager);
+    auto catchingLocation = entityManager->createEntity("catchingLocation");
+    catchingLocation->addComponent<PositionComponent>(0, 0);
+    catchingLocation->addComponent<SizeComponent>(WINDOW_WIDTH, WINDOW_HEIGHT);
+	catchingLocation->addComponent<RenderLayerComponent>(0);
+    catchingLocation->addComponent<CatchingTypeEntityComponent>();
+    sf::Texture catchingLocationTexture;
+    if (catchingLocationTexture.loadFromFile("../res/beach(383x640).png")) {
+        catchingLocation->addComponent<TextureComponent>(
+			catchingLocationTexture,
+			CATCHING_LOCATION_WIDTH, 
+			CATCHING_LOCATION_HEIGHT
+		);
     }
+}
 
-    float x = ARROW_POS_X - ARROW_RADIUS * cosf(rotation->angle + PI / 2);
-    float y = ARROW_POS_Y - ARROW_RADIUS * sinf(rotation->angle + PI / 2);
-
-    // Устанавливаем новую позицию стрелки
-    position->setPos(x, y);
+void initCatchingArrow(EntityManager* entityManager) {
+    auto catchingArrow = entityManager->createEntity("catchingArrow");
+    catchingArrow->addComponent<PositionComponent>(
+        ARROW_POS_X, 
+        ARROW_POS_Y
+    );
+    catchingArrow->addComponent<SizeComponent>(ARROW_WIDTH, ARROW_HEIGHT);
+	catchingArrow->addComponent<RotationComponent>(ARROW_SPEED, ARROW_WIDTH / 2, ARROW_HEIGHT + ARROW_POKEBALL_GAP + POKEBALL_HEIGHT * 5 / 2);
+	catchingArrow->addComponent<RenderLayerComponent>(1);
+    catchingArrow->addComponent<CatchingTypeEntityComponent>();
+    sf::Texture catchingArrowTexture;
+    if (catchingArrowTexture.loadFromFile("../res/arrow(57x162).png")) {
+        catchingArrow->addComponent<TextureComponent>(
+			catchingArrowTexture,
+			ARROW_WIDTH, 
+			ARROW_HEIGHT
+		);
+    }
+    catchingArrow->getComponent<TextureComponent>()->sprite.setOrigin(ARROW_WIDTH / 2, ARROW_HEIGHT);
 }
 
 void onSpacePressed(EntityManager* entityManager, InputSystem* inputSystem, float dt) {
@@ -155,44 +176,49 @@ void throwPokeball(EntityManager* entityManager, float dt) {
 }
 
 void pokeballCollision(EntityManager* entityManager, RenderSystem* renderSystem, GameState* state) {
+    //  TODO: сократить минимум в 2 раза
+    auto pokemon = entityManager->getEntitiesWithComponent<AttackedPokemonComponent>()[0];
+    auto pokeball = entityManager->getEntity("pokeball");
+    if (isCollision(pokeball, pokemon)) {
+        handlePokeballPokemonCollision(entityManager, renderSystem, state);
+    }
+    auto location = entityManager->getEntity("catchingLocation");
+    if (pokeball != nullptr && location != nullptr && !isCollision(pokeball, location)) {
+        handlePokeballOutOfMap(entityManager, renderSystem, state);
+    }
+}
+
+void handlePokeballOutOfMap(EntityManager* entityManager, RenderSystem* renderSystem, GameState* state) {
     auto pokeball = entityManager->getEntity("pokeball");
     auto location = entityManager->getEntity("catchingLocation");
     auto pokemon = entityManager->getEntitiesWithComponent<AttackedPokemonComponent>()[0];
-    if (isCollision(pokeball, pokemon)) {
-        auto player = entityManager->getEntity("player");
-        auto inventory = player->getComponent<PlayersInventoryComponent>();
-        inventory->addPokemon(pokemon->getId());
-        pokemon->getComponent<PokemonComponent>()->setCollected(true);
-        
-        pokemon->removeComponent<CatchingTypeEntityComponent>();
 
-        removeCatchingEntities(entityManager, renderSystem);
+    pokemon->removeComponent<CatchingTypeEntityComponent>();
+    setPokemonPropsIfLose(pokemon);
+    removeCatchingEntities(entityManager, renderSystem);
+    
+    auto entities = entityManager->getEntitiesWithComponent<GameTypeEntityComponent>();
+    renderSystem->addEntities(entities);
 
-        auto entities = entityManager->getEntitiesWithComponent<GameTypeEntityComponent>();
-        renderSystem->addEntities(entities);
-
-        *state = GameState::Game;
-    }
-    pokeball = entityManager->getEntity("pokeball");
-    location = entityManager->getEntity("catchingLocation");
-    if (pokeball != nullptr && location != nullptr && !isCollision(pokeball, location)) {
-        pokemon->removeComponent<CatchingTypeEntityComponent>();
-        removeCatchingEntities(entityManager, renderSystem);
-        pokemon->getComponent<PositionComponent>()->setPos(
-            BULBASOUR_POSITION_X,
-            BULBASOUR_POSITION_Y
-        );
-        pokemon->getComponent<SizeComponent>()->setSize(
-            POKEMON_INVENTORY_WIDTH,
-            POKEMON_INVENTORY_HEIGHT
-        );
-        pokemon->addComponent<GameTypeEntityComponent>();
-        auto entities = entityManager->getEntitiesWithComponent<GameTypeEntityComponent>();
-        renderSystem->addEntities(entities);
-
-        *state = GameState::Game;
-    }
+    *state = GameState::Game;
 }
+
+void handlePokeballPokemonCollision(EntityManager* entityManager, RenderSystem* renderSystem, GameState* state) {
+    auto pokemon = entityManager->getEntitiesWithComponent<AttackedPokemonComponent>()[0];
+    pokemon->removeComponent<CatchingTypeEntityComponent>();
+    auto player = entityManager->getEntity("player");
+    auto inventory = player->getComponent<PlayersInventoryComponent>();
+    inventory->addPokemon(pokemon->getId());
+    pokemon->getComponent<PokemonComponent>()->setCollected(true);
+    
+    removeCatchingEntities(entityManager, renderSystem);
+
+    auto entities = entityManager->getEntitiesWithComponent<GameTypeEntityComponent>();
+    renderSystem->addEntities(entities);
+
+    *state = GameState::Game;
+}
+
 
 void removeCatchingEntities(EntityManager* entityManager, RenderSystem* renderSystem) {
     auto entities = entityManager->getEntitiesWithComponent<CatchingTypeEntityComponent>();
@@ -200,4 +226,16 @@ void removeCatchingEntities(EntityManager* entityManager, RenderSystem* renderSy
     for (auto entity : entities) {
         entityManager->removeEntity(entity->getId());
     }
+}
+
+void setPokemonPropsIfLose(Entity *pokemon) {
+    pokemon->getComponent<PositionComponent>()->setPos(
+        BULBASOUR_POSITION_X,
+        BULBASOUR_POSITION_Y
+    );
+    pokemon->getComponent<SizeComponent>()->setSize(
+        POKEMON_INVENTORY_WIDTH,
+        POKEMON_INVENTORY_HEIGHT
+    );
+    pokemon->addComponent<GameTypeEntityComponent>();
 }
--- Pokemon/PokemonSystem.h
+++ Pokemon/PokemonSystem.h
@@ -6,6 +6,19 @@
 #include "../Catching/CatchingSystem.h"
 #include "../GameState.h"
 #include "../const.h"
+
+struct ConditionProps {
+	Entity* pokemon;
+	Entity* player;
+	PlayersInventoryComponent* inventory;
+	std::vector<sf::Keyboard::Key> keys;
+};
+
+bool isInventoryFull(PlayersInventoryComponent *inventory);
+bool isPokemonCollected(Entity* pokemon);
+bool isEnterPressed(std::vector<sf::Keyboard::Key> keys);
+bool doesConditionSatisfy(ConditionProps props);
+
 void pokemonCollision(
 	InputSystem* inputSystem,
 	EntityManager* entityManager,
@@ -18,12 +31,12 @@ void pokemonCollision(
 	auto inventory = player->getComponent<PlayersInventoryComponent>();
 	auto keys = inputSystem->getPressedKeys();
 	for (auto pokemon : pokemons) {
-		if (
-			isCollision(pokemon, player) 
-			&& inventory->getPokemonCount() < INVENTORY_CELLS_LIMIT
-			&& !pokemon->getComponent<PokemonComponent>()->isCollected()
-			&& std::find(keys.begin(), keys.end(), sf::Keyboard::Enter) != keys.end()
-		) {
+		if (doesConditionSatisfy({ // условие должно быть простым
+				pokemon, 
+				player,
+				inventory,
+				keys
+			})) {
 			*state = GameState::Catching;
 			inputSystem->clear();
 			pokemon->addComponent<AttackedPokemonComponent>();
@@ -32,4 +45,23 @@ void pokemonCollision(
 			initCatching(entityManager, renderSystem);
 		}
 	}
+}
+
+bool doesConditionSatisfy(ConditionProps props) {
+	return isCollision(props.pokemon, props.player) 
+		&& isInventoryFull(props.inventory)
+		&& isPokemonCollected(props.pokemon)
+		&& isEnterPressed(props.keys);
+}
+
+bool isEnterPressed(std::vector<sf::Keyboard::Key> keys) {
+	return std::find(keys.begin(), keys.end(), sf::Keyboard::Enter) != keys.end();
+}
+
+bool isInventoryFull(PlayersInventoryComponent *inventory) {
+	return inventory->getPokemonCount() < INVENTORY_CELLS_LIMIT;
+}
+
+bool isPokemonCollected(Entity* pokemon) {
+	return !pokemon->getComponent<PokemonComponent>()->isCollected();
 }
