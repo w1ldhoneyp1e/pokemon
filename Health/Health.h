ddaad06c6a1fcfc651568b7f438385ce61db2e71
w1ldhoneyp1e <k_yashmetov@mail.ry>
2025-01-13 16:59:31 +0300

    Поправил отображение жизни

--- Health/Health.h
+++ Health/Health.h
@@ -1,15 +1,17 @@
 #include <SFML/Graphics.hpp>
 #include "../Components/Components.h"
 #include "../Entity.h"
+#include "../const.h"
 
 float getWidth(float length, HealthComponent *healthComp);
 
 void showHealth(sf::RenderWindow *window, float length, float height, HealthComponent *healthComp, PositionComponent *positionComp, float scale) {
+	float offsetX = SCREEN_WIDTH / 2 - scale * WINDOW_WIDTH / 2;
 	sf::RectangleShape rectangle(sf::Vector2f(getWidth(length, healthComp), height * 0.1));
     rectangle.setFillColor(sf::Color::Green);
     rectangle.setOutlineColor(sf::Color::Black);
     rectangle.setOutlineThickness(.5f);
-    rectangle.setPosition(positionComp->getX() * scale, (positionComp->getY()) * scale);
+    rectangle.setPosition(positionComp->getX() * scale + offsetX, (positionComp->getY()) * scale);
 	window->draw(rectangle);
 }
 
