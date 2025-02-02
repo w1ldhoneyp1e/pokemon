2f3269c3579eb8d359d2c2e47dd13ce7a9e38e97
w1ldhoneyp1e <k_yashmetov@mail.ry>
2025-02-02 18:50:51 +0300

    Изменил проверку коллизий

--- systems/CollisionSystem.h
+++ systems/CollisionSystem.h
@@ -1,8 +1,23 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 #include "../Entity.h"
+#include "../Components/BaseEntityComponents.h"
+
 bool isCollision(Entity* entity1, Entity* entity2) {
-    sf::FloatRect rect1 = entity1->getComponent<TextureComponent>()->sprite.getGlobalBounds();
-    sf::FloatRect rect2 = entity2->getComponent<TextureComponent>()->sprite.getGlobalBounds();
-    return rect1.intersects(rect2);
+    auto pos1 = entity1->getComponent<PositionComponent>();
+    auto size1 = entity1->getComponent<SizeComponent>();
+    auto pos2 = entity2->getComponent<PositionComponent>();
+    auto size2 = entity2->getComponent<SizeComponent>();
+    
+    float left1 = pos1->getX();
+    float right1 = pos1->getX() + size1->getWidth();
+    float top1 = pos1->getY();
+    float bottom1 = pos1->getY() + size1->getHeight();
+    
+    float left2 = pos2->getX();
+    float right2 = pos2->getX() + size2->getWidth();
+    float top2 = pos2->getY();
+    float bottom2 = pos2->getY() + size2->getHeight();
+    
+    return !(right1 < left2 || left1 > right2 || bottom1 < top2 || top1 > bottom2);
 }
