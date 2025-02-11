51efd10c9cfa0b4dfa78e02cbbb7e93f00a9cda6
w1ldhoneyp1e <k_yashmetov@mail.ry>
2025-02-11 15:40:47 +0300

    Убрал попытку загрузить карту, которой не будет

--- main.cpp
+++ main.cpp
@@ -154,7 +154,6 @@ int main() {
 	SCALE = std::min(scaleX, scaleY);
 
 	collisionMaps.emplace(LocationType::Town, CollisionMap("../res/collisionMap/town.txt", WINDOW_WIDTH / 76, WINDOW_HEIGHT / 64));
-	collisionMaps.emplace(LocationType::Forest, CollisionMap("../res/collisionMap/forest.txt", WINDOW_WIDTH / 38, WINDOW_HEIGHT / 32));
 	Controller controller(&entityManager, &inputSystem, &renderSystem, &state, &battleContext, &collisionMaps, LocationType::Town);
 
 	initMenu(&entityManager, &renderSystem);
